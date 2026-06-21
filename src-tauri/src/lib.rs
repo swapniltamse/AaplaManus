49e3f6eae709490d4e72060b87fbf6cde0915cc5
Swapnil <stamsepublic@gmail.com>
2026-06-21 11:12:05 -0400

    fix lib.rs stub: register shell and single-instance plugins

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
+        .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("failed to run app");
 }
